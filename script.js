991f48f5539cebbfd4c2c2bafa1779f3c9aef4a9
Hale Holman <haleonearth@users.noreply.github.com>
2023-04-11 20:27:38 +0000

    Simplified and functioning.

--- script.js
+++ script.js
@@ -1,59 +1,29 @@
-/* This script simply changes the data attribute 'selected',
-but doesn't handle any animation. All animation is done in CSS. 
-If the target element's 'data-sync' attribute is true, only
-one accordion can be opened at a time. */
+const accordionItems = document.querySelectorAll('.hs-accordion__item');
 
-function buildAccordion(el) {
+accordionItems.forEach(accordionItem => {
+  const accordionButton = accordionItem.querySelector('button');
 
-  var accordion = el,
-    accordionItems = accordion.children,
-    oneAtTime = accordion.getAttribute('data-sync'),
-    hideAllItems = function(ex) {
-      var activeItems = accordion.querySelectorAll('.hs-accordion__item[aria-expanded="true"]');
-      Array.prototype.forEach.call(accordionItems, function(el, i) {
-        if (el !== ex && (activeItems.length > 1 || !el.classList.contains('active'))) {
-          el.setAttribute('aria-expanded', 'false');
-          el.classList.remove('active');
-        }
-      });
-    },
-    toggleItem = function(el) {
-      /* Toggle whether item is selected or not */
-      if (el.getAttribute('aria-expanded') == 'true') {
-        if (oneAtTime) {
-          hideAllItems(el);
-          return;
-        }
-        el.setAttribute('aria-expanded', 'false');
+  accordionButton.addEventListener('click', () => {
+    accordionItems.forEach(item => {
+      const itemContent = item.querySelector('.hs-accordion__item-content');
+
+      if (item !== accordionItem) {
+        item.classList.remove('active');
+        itemContent.classList.remove('active');
+        itemContent.style.height = '0';
       }
-      else {
-        if (oneAtTime) {
-          hideAllItems(el);
-        }
-        el.setAttribute('aria-expanded', 'true');
-      }
-    },
-    clickBind = function() {
-      accordion.addEventListener('click', function(e) {
-        if (e.target && e.target.nodeName == "BUTTON") {
-          /* Should only 1 item show? */
-          if (oneAtTime) {
-            hideAllItems(e.target.parentNode);
-          }
-          /* Toggle the item */
-          toggleItem(e.target.parentNode);
-          e.target.parentNode.classList.toggle('active'); // add this line
-        }
-      });
-    },
-    /* Go time */
-    init = (function() {
-      clickBind();
-    })();
-}
+    });
 
-var accordions = document.querySelectorAll('.hs-accordion');
-Array.prototype.forEach.call(accordions, function(el) {
-  buildAccordion(el);     /* Find all instances of '.hs-accordion' and initialize for each of them */
+    const accordionContent = accordionItem.querySelector('.hs-accordion__item-content');
+
+    if (accordionItem.classList.contains('active')) {
+      accordionItem.classList.remove('active');
+      accordionContent.classList.remove('active');
+      accordionContent.style.height = '0';
+    } else {
+      accordionItem.classList.add('active');
+      accordionContent.classList.add('active');
+      accordionContent.style.height = accordionContent.scrollHeight + 'px';
+    }
+  });
 });
-
